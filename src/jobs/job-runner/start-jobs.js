6da3dcd44be7ad5ee6433aa9897d432cc4c2decb
Suraj Shrestha <syuraj@gmail.com>
2020-10-06 23:52:45 -0400

    removed unused jobs

--- src/jobs/job-runner/start-jobs.js
+++ src/jobs/job-runner/start-jobs.js
@@ -3,11 +3,8 @@ const Agenda = require('agenda')
 const logger = require('../../config/logger')
 
 const crawler = require('../NewsCrawlerTrigger/index')
-const notifier = require('../NotificationTrigger')
 const twitterJob = require('../TwitterTrigger')
 const trendingJob = require('../trending')
-const facebookPost = require('../facebookPost')
-const trendingTopic = require('../trendingTopics')
 
 module.exports = async function () {
 	logger.info('starting jobs')
@@ -19,37 +16,20 @@ module.exports = async function () {
 		crawler()
 	})
 
-	agenda.define('notify users', async (job) => {
-		logger.info('notify users job started')
-		notifier(console)
-	})
-
 	agenda.define('pull tweets', async (job) => {
 		logger.info('pull tweets job started')
 		twitterJob()
 	})
 
-	agenda.define('fetch trending', async (job) => {
+	agenda.define('fetch trending', async () => {
 		logger.info('fetch trending job started')
 		trendingJob()
-	})
-
-	agenda.define('post to facebook', async (job) => {
-		logger.info('posting to facebook started')
-		facebookPost(console)
-	})
-
-	agenda.define('fetch trending topics', async (job) => {
-		logger.info('fetch trending topics started')
-		trendingTopic()
 	})
 
 	await agenda.start()
 
 	await agenda.every('30 minutes', 'crawl articles')
 	await agenda.every('5 minutes', 'notify users')
-	await agenda.every('10 minutes', 'pull tweets')
+	await agenda.every('20 minutes', 'pull tweets')
 	await agenda.every('1 hours', 'fetch trending')
-	await agenda.every('136 minutes', 'post to facebook')
-	await agenda.every('24 hours', 'fetch trending topics')
 }
