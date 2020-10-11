76014d13439cd09634045df53b9d079b58e6f81c
Suraj Shrestha <syuraj@gmail.com>
2020-10-11 00:31:57 -0400

    news crawl error fix

--- src/jobs/NewsCrawlerTrigger/index.js
+++ src/jobs/NewsCrawlerTrigger/index.js
@@ -2,7 +2,6 @@ const NewsCrawler = require('news-crawler')
 const { saveArticles } = require('../../db-service/newsDbService')
 const SourceConfig = require('../../config/news-source-config.json')
 const logger = require('../../config/logger')
-const { filterNewArticles } = require('./helper')
 const { Article } = require('../../db-service/database/mongooseSchema')
 
 module.exports = async function () {
@@ -12,13 +11,13 @@ module.exports = async function () {
 		let articles = await NewsCrawler(SourceConfig, 3)
 		articles = articles.filter((a) => a.imageLink !== null)
 
-		const savedArticles = await Article.find({ createdDate: { $gt: new Date(Date.now() - 12 * 60 * 60 * 1000) } })
+		const savedArticles = await Article.find({ createdDate: { $gt: new Date(Date.now() - 24 * 60 * 60 * 1000) } })
 
-		const checkWithOldArticles = filterNewArticles(articles, savedArticles)
+		const newArticles = articles.filter((article) => !savedArticles.some((sa) => sa.link === article.link))
 
-		checkWithOldArticles.forEach((x) => (x.hostIp = ipAddress))
+		newArticles.forEach((x) => (x.hostIp = ipAddress))
 
-		await saveArticles(checkWithOldArticles)
+		await saveArticles(newArticles)
 	} catch (error) {
 		logger.error('Error while crawling:', error)
 	}
